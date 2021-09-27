219fb3bee536d3d3a99066e58af70d4ff58d91e1
kikizq <280901150@qq.com>
2021-09-27 16:40:37 +0800

    2-3 修改启动文案

--- src/main/java/com/kiki/wiki/WikiApplication.java
+++ src/main/java/com/kiki/wiki/WikiApplication.java
@@ -1,13 +1,23 @@
 package com.kiki.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class WikiApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikiApplication.class);
+
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOGGER.info("启动成功！！");
+        LOGGER.info("地址：http://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 
