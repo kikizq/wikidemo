a9bef709aa116ae74daa87973f2783e3527ac55b
kikizq <280901150@qq.com>
2021-09-30 14:07:23 +0800

    2-8 新增表，生成持久层代码

---
+++ src/main/java/com/kiki/wiki/domain/Ebook.java
@@ -0,0 +1,57 @@
+package com.kiki.wiki.domain;
+
+public class Ebook {
+    private Long id;
+
+    private String name;
+
+    private Long category1Id;
+
+    private Long category2Id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCategory1Id() {
+        return category1Id;
+    }
+
+    public void setCategory1Id(Long category1Id) {
+        this.category1Id = category1Id;
+    }
+
+    public Long getCategory2Id() {
+        return category2Id;
+    }
+
+    public void setCategory2Id(Long category2Id) {
+        this.category2Id = category2Id;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", category1Id=").append(category1Id);
+        sb.append(", category2Id=").append(category2Id);
+        sb.append("]");
+        return sb.toString();
+    }
+}
