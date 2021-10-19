7b4f37953dc731d6e83f3bfa81544ccf1ee1400b
kikizq <280901150@qq.com>
2021-10-19 18:00:33 +0800

    3-3 用户自定义组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        kiki ebook
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'TheFooter',
+        props: {
+            msg: String,
+        },
+    });
+</script>
