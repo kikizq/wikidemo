5f07c82888391e3c95c6833470ed7df81e4f7474
kikizq <280901150@qq.com>
2021-10-21 14:49:42 +0800

    3-6 ant vue list

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,11 +48,32 @@
         <a-layout-content
                 :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-            <pre>
-{{ebooks}}
-                <!--{{books}}-->
-            </pre>
-
+            <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+                <template #renderItem="{ item }">
+                    <a-list-item key="item.title">
+                        <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+                        </template>
+                        <template #extra>
+                            <img
+                                    width="272"
+                                    alt="logo"
+                                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                            />
+                        </template>
+                        <a-list-item-meta :description="item.description">
+                            <template #title>
+                                <a :href="item.href">{{ item.title }}</a>
+                            </template>
+                            <template #avatar><a-avatar :src="item.avatar" /></template>
+                        </a-list-item-meta>
+                        {{ item.content }}
+                    </a-list-item>
+                </template>
+            </a-list>
         </a-layout-content>
     </a-layout>
 </template>
@@ -61,6 +82,19 @@
 import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
 import axios from 'axios'; // @ is an alias to /src
 
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+    listData.push({
+        href: 'https://www.antdv.com/',
+        title: `ant design vue part ${i}`,
+        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        description:
+            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content:
+            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    });
+}
 export default defineComponent({
     name: 'Home',
     setup() {
@@ -77,9 +111,23 @@ export default defineComponent({
                 console.log(response);
             });
         });
+        const pagination = {
+            onChange: (page: number) => {
+                console.log(page);
+            },
+            pageSize: 3,
+        };
+        const actions: Record<string, string>[] = [
+            { type: 'StarOutlined', text: '156' },
+            { type: 'LikeOutlined', text: '156' },
+            { type: 'MessageOutlined', text: '2' },
+        ];
         return {
             ebooks,
             // books: toRef(ebooks1, "books")
+            listData,
+            pagination,
+            actions,
         }
     }
 });
