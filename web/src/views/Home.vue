33688984d09db3103a8cc19abbaf0add8d5a4067
kikizq <280901150@qq.com>
2021-10-21 17:35:16 +0800

    3-10 配置axios 拦截器

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -101,7 +101,6 @@ export default defineComponent({
                 const data = response.data;
                 ebooks.value = data.content;
                 // ebooks1.books = data.content;
-                console.log(response);
             });
         });
         const pagination = {
