588e8123b297ef2415557f0f77aa2f79e77831d6
kikizq <280901150@qq.com>
2021-10-21 17:45:09 +0800

    3-10 配置过滤器打印日志

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -10,7 +10,7 @@
                 <a-sub-menu key="sub1">
                     <template #title>
               <span>
-                <user-outlined />
+                <user-outlined/>
                 subnav 1
               </span>
                     </template>
@@ -22,7 +22,7 @@
                 <a-sub-menu key="sub2">
                     <template #title>
               <span>
-                <laptop-outlined />
+                <laptop-outlined/>
                 subnav 2
               </span>
                     </template>
@@ -34,7 +34,7 @@
                 <a-sub-menu key="sub3">
                     <template #title>
               <span>
-                <notification-outlined />
+                <notification-outlined/>
                 subnav 3
               </span>
                     </template>
@@ -53,7 +53,7 @@
                     <a-list-item key="item.name">
                         <template #actions>
                           <span v-for="{ type, text } in actions" :key="type">
-                            <component v-bind:is="type" style="margin-right: 8px" />
+                            <component v-bind:is="type" style="margin-right: 8px"/>
                             {{ text }}
                           </span>
                         </template>
@@ -61,7 +61,9 @@
                             <template #title>
                                 <a :href="item.href">{{ item.name }}</a>
                             </template>
-                            <template #avatar><a-avatar :src="item.cover" /></template>
+                            <template #avatar>
+                                <a-avatar :src="item.cover"/>
+                            </template>
                         </a-list-item-meta>
                     </a-list-item>
                 </template>
@@ -71,58 +73,56 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
-import axios from 'axios'; // @ is an alias to /src
+    import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
+    import axios from 'axios'; // @ is an alias to /src
 
-const listData: Record<string, string>[] = [];
+    const listData: Record<string, string>[] = [];
 
-for (let i = 0; i < 23; i++) {
-    listData.push({
-        href: 'https://www.antdv.com/',
-        title: `ant design vue part ${i}`,
-        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        description:
-            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-        content:
-            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-    });
-}
-export default defineComponent({
-    name: 'Home',
-    setup() {
-        console.log("setup");
-        const ebooks = ref();
-        // const ebooks1 = reactive({books: []});
-
-        onMounted(() => {
-            console.log("onMounted")
-            console.log("http" + process.env.VUE_APP_SERVER)
-            axios.get("/ebook/list").then((response) => {
-                const data = response.data;
-                ebooks.value = data.content;
-                // ebooks1.books = data.content;
-            });
+    for (let i = 0; i < 23; i++) {
+        listData.push({
+            href: 'https://www.antdv.com/',
+            title: `ant design vue part ${i}`,
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
         });
-        const pagination = {
-            onChange: (page: number) => {
-                console.log(page);
-            },
-            pageSize: 3,
-        };
-        const actions: Record<string, string>[] = [
-            { type: 'StarOutlined', text: '156' },
-            { type: 'LikeOutlined', text: '156' },
-            { type: 'MessageOutlined', text: '2' },
-        ];
-        return {
-            ebooks,
-            // books: toRef(ebooks1, "books")
-            listData,
-            pagination,
-            actions,
-        }
     }
-});
+    export default defineComponent({
+        name: 'Home',
+        setup() {
+            const ebooks = ref();
+            // const ebooks1 = reactive({books: []});
+
+            onMounted(() => {
+                console.log("http" + process.env.VUE_APP_SERVER)
+                axios.get("/ebook/list").then((response) => {
+                    const data = response.data;
+                    ebooks.value = data.content;
+                    // ebooks1.books = data.content;
+                });
+            });
+            const pagination = {
+                onChange: (page: number) => {
+                    console.log(page);
+                },
+                pageSize: 3,
+            };
+            const actions: Record<string, string>[] = [
+                {type: 'StarOutlined', text: '156'},
+                {type: 'LikeOutlined', text: '156'},
+                {type: 'MessageOutlined', text: '2'},
+            ];
+            return {
+                ebooks,
+                // books: toRef(ebooks1, "books")
+                listData,
+                pagination,
+                actions,
+            }
+        }
+    });
 </script>
 
 <style scoped>
