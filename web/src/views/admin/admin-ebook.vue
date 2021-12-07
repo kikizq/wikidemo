4b5451942fbd3a761ebbcee5d2a17866e421183f
kikizq <280901150@qq.com>
2021-12-07 18:26:40 +0800

    4-5 电子书页面前后端分页整合完成

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -101,7 +101,7 @@
             const ebooks = ref();
             const pagination = ref({
                 current: 1,
-                pageSize: 10,
+                pageSize: 4,
                 total: 0
             });
             const loading = ref(false);
@@ -145,16 +145,16 @@
                 // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
                 ebooks.value = [];
                 axios.get("/ebook/list", {
-                    // params: {
-                    //     page: params.page,
-                    //     size: params.size,
-                    //     name: param.value.name
-                    // }
+                    params: {
+                        page: params.page,
+                        size: params.size,
+                        // name: param.value.name
+                    }
                 }).then((response) => {
                     loading.value = false;
                     const data = response.data;
                     if (data.success) {
-                        ebooks.value = data.content;
+                        ebooks.value = data.content.list;
                         // 重置分页按钮
                         pagination.value.current = params.page;
                         pagination.value.total = data.content.total;
@@ -268,7 +268,10 @@
             };
             onMounted(() => {
                 // handleQueryCategory();
-                handleQuery({});
+                handleQuery({
+                    page: 1,
+                    size: pagination.value.pageSize
+                });
             });
             return {
                 param,
